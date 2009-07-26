b55ab70649400f43e6c26e022f0760165d42a211
Jesse Andrews <anotherjesse@gmail.com>
2009-07-26 15:21:30 -0700

    terminate loops early when we know it won't work - 4s solution now

--- 031/coins.rb
+++ 031/coins.rb
@@ -3,13 +3,17 @@ $coin_values = [1, 2, 5, 10, 20, 50, 100, 200]
 $ans = 0
 
 def coinbox(current, current_coin_idx, change)
-  return if current > 200
   if current_coin_idx == $coin_values.size
-    if current == 200
-      $ans += 1
-    end
+    $ans += 1 if current == 200
     return
   end
+
+  # use the fact that if a coin divides all coins of great value
+  # then it must evenly divide the change we need to make
+  if [5,10,50,100,200].include? $coin_values[current_coin_idx]
+    return unless (200 - current) % $coin_values[current_coin_idx] == 0
+  end
+
   max_coins = (200 - current) / $coin_values[current_coin_idx]
   (0..max_coins).each do |coins|
     coinbox(current + ($coin_values[current_coin_idx] * coins), current_coin_idx + 1, change + [coins])
